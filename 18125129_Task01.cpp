a7135f7eb5c7cad40777798e1d0b7abc302c664a
Hildford <nthau18@apcs.vn>
2021-04-01 12:03:23 +0700

    add task 1 & 2

---
+++ 18125129_Task01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//Task 01: listing items
+int main(){
+    string ter[100] ={"colon", "ID", "COMMA"};
+    string nonTer[100];
+    return 18125129;
+}
---
+++ 18125129_Task02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//Task 02: Find the useless symbol
+//Symbol A is not useless if there is a derivation starting from S in which A appears
+//and the derivation leads to a string of terminals or the empty string (#)
+int main(){
+    string ter[100] ={"colon", "ID", "COMMA"};
+    string nonTer[100];
+    string examples = {""}
+    return 18125129;
+}
